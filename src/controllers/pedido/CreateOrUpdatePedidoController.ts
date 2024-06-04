8c7b89c6b6d8c7dc76a3ccd680352cae1a871ca5
Danilo Marinho <dannilodms@gmail.com>
2024-06-04 16:46:14 -0300

    feat: adicionando token de autenticação

--- src/controllers/pedido/CreateOrUpdatePedidoController.ts
+++ src/controllers/pedido/CreateOrUpdatePedidoController.ts
@@ -1,13 +1,13 @@
 import HttpRequest from '@/adapters/interfaces/http/HttpRequest';
 import HttpResponse from '@/adapters/interfaces/http/HttpResponse';
 
-import { ok } from '@/helpers/HttpHelpers';
+import { ok, unauthorized } from '@/helpers/HttpHelpers';
 import Controller from '../interfaces/Controller';
 
 export default class CreateOrUpdatePedidoController implements Controller {
   async handle(req: HttpRequest): Promise<HttpResponse<any>> {
-    console.log(req.headers);
     await new Promise(resolve => setTimeout(resolve, 1200));
+    if (req.headers?.authorization !== 'api-homolog') return unauthorized('Token de autorização inválido');
     return ok(`Pedido ${req?.params?.pedido} filial ${req?.params?.filial} atualizado com sucesso!`);
   }
 }
